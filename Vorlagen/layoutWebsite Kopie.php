1f9e591e49ecb338cdeb36d3df16924359f9a84c
Nocsis <nicoslentzsch@web.de>
2019-06-18 12:15:27 +0200

    fixed language slection scripts, updated stylesheet

--- Vorlagen/layoutWebsite Kopie.php
+++ Vorlagen/layoutWebsite Kopie.php
@@ -6,7 +6,6 @@
     <title>Tor nach Zion</title>
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <link rel="stylesheet" href="../stylesheet.css">
-    <script src="../scripts.js"></script>
   </head>
 
   <body>
@@ -29,7 +28,7 @@
         <img src="../Material/Bilder/portrait-dummy.jpg" alt="portrait-dummy" height="100%" width="90.91%">
       </div>
       
-      <div id="text">
+      <div class="text de">
         Lorem ipsum dolor sit amet, consetetur sadipscing elitr, 
         sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, 
         sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, 
@@ -37,6 +36,10 @@
         sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et
         accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor 
         sit amet.
+      </div>
+
+      <div class="text en">
+        English blind text here.
       </div>
 
       <div id="jahr">year here</div>
@@ -48,6 +51,8 @@
 
     <button class="scrollButton" id="downButton" onclick="scrollToBottom()"></button>
     <button class="scrollButton" id="upButton" onclick="scrollToTop()"></button>
+
+    <script src="../scripts.js"></script>
   </body>
 
 </html>
--- scripts.js
+++ scripts.js
@@ -1,9 +1,26 @@
 "use strict";
 
+var deutsch = document.getElementById('de_click'),
+    english = document.getElementById('en_click'),
+    de_txt = document.querySelectorAll('.de'),
+    en_txt = document.querySelectorAll('.en'),
+    nb_de = de_txt.length,
+    nb_en = en_txt.length;
+
+deutsch.addEventListener('click', function() {
+    language(deutsch,english);
+    });
+
+english.addEventListener('click', function() {
+    language(english,deutsch);
+    });
+
 init();
 
 window.onload = function() {checkScroll()};
 window.onscroll = function() {checkScroll()};
+
+
 
 function checkScroll() {
     if (document.body.scrollTop > (document.body.scrollHeight / 4) || document.documentElement.scrollTop > (document.documentElement.scrollHeight / 4)) {
@@ -25,51 +42,34 @@ function scrollToBottom() {
     window.scroll({top: document.documentElement.scrollHeight, behavior: 'smooth'});
 }
 
-var deutsch = document.getElementById('de_click'),
-        english = document.getElementById('en_click'),
-        de_txt = document.querySelectorAll('#de'),
-        en_txt = document.querySelectorAll('#en'),
-        nb_de = de_txt.length,
-        nb_en = en_txt.length;
-
-        deutsch.addEventListener('click', function() {
-        language(deutsch,english);
-        }, false);
-
-        english.addEventListener('click', function() {
-        language(english,deutsch);
-        }, false);
-
-        function language(languageOn,languageOff){
-             if (!languageOn.classList.contains('current_lang')) {
-                languageOn.classList.toggle('current_lang');
-                languageOff.classList.toggle('current_lang');
-                }
-
-                if(languageOn.innerHTML == 'De'){
-                display(de_txt, nb_de);
-                hide(en_txt, nb_en);
-                }
-
-                else if(languageOn.innerHTML == 'En'){
-                display(en_txt, nb_en);
-                 hide(de_txt, nb_de);
-                 }   
-            }
-
-        function display(txt,nb){
-            for(var i=0; i < nb; i++){
-                txt[i].style.display = 'block';
-            }
+function init(){
+    language(deutsch,english);
+}
+function language(languageOn,languageOff){
+    if (!languageOn.classList.contains('current_lang')) {
+        languageOn.classList.toggle('current_lang');
+        languageOff.classList.toggle('current_lang');
         }
 
-        function hide(txt,nb){
-            for(var i=0; i < nb; i++){
-                txt[i].style.display = 'none';
-            }
+        if(languageOn.innerHTML == 'DE'){
+        display(de_txt, nb_de);
+        hide(en_txt, nb_en);
         }
 
-        function init(){
-            language(deutsch,english);
-        }
-        
+        else if(languageOn.innerHTML == 'EN'){
+        display(en_txt, nb_en);
+        hide(de_txt, nb_de);
+        }   
+    }
+
+function display(txt,nb){
+    for(var i=0; i < nb; i++){
+        txt[i].style.display = 'block';
+    }
+}
+
+function hide(txt,nb){
+    for(var i=0; i < nb; i++){
+        txt[i].style.display = 'none';
+    }
+}       
