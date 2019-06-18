5820d594f8905222ae9af95419ef43ff29d9354a
Nocsis <nicoslentzsch@web.de>
2019-06-18 12:14:46 +0200

    fixed nav formatting, changed language selectors to buttons

--- html/header.php
+++ html/header.php
@@ -1,30 +1,31 @@
 <header>
 <div class="flex-container">
-      <div id="RandLinks"></div>
-      <div id="ueberleben">Über<strong>leben</strong> </div>
+  <div id="randLinks"></div>
+  <div id="ueberleben">Über<strong>leben</strong> </div>
       
-      <div id="navigation">  
-        <a href="#">BELLA&nbsp; //</a>
-        <a href="#">&nbsp;&nbsp;JOSEPH &nbsp;//</a>
-        <a href="#">&nbsp;&nbsp;NATHAN&nbsp; //</a>
-        <a href="#">&nbsp;&nbsp;STEFFI &nbsp;//</a>
-        <a href="#">&nbsp;&nbsp;AVISHAI &nbsp;//</a>
-        <a href="#">&nbsp;&nbsp;CARLA &nbsp; //</a>
-      </nav></div>
+  <div id="navigation">
+    <nav>  
+      <a href="#">BELLA&nbsp; //</a>
+      <a href="#">&nbsp;&nbsp;JOSEPH &nbsp;//</a>
+      <a href="#">&nbsp;&nbsp;NATHAN&nbsp; //</a>
+      <a href="#">&nbsp;&nbsp;STEFFI &nbsp;//</a>
+      <a href="#">&nbsp;&nbsp;AVISHAI &nbsp;//</a>
+      <a href="#">&nbsp;&nbsp;CARLA &nbsp; //</a>
+    </nav>
+  </div>
 
-      <div id="navigationRechts"> 
-        <nav class="sidenav">
-           <a href="#"><!--&emsp;&emsp;&emsp;&nbsp;-->FILM&nbsp;&nbsp;//</a>
-           <a href="#">&nbsp;&nbsp;VEREIN&nbsp;&nbsp;//</a>
-           <a href="#">&nbsp;&nbsp;KONTAKT&nbsp;&nbsp;//</a>
-           <a href="#">&nbsp;&nbsp;IMPRESSUM </a>
-         </nav>
-      </div> 
+  <div id="navigationRechts"> 
+    <nav class="sidenav">
+      <a href="#"><!--&emsp;&emsp;&emsp;&nbsp;-->FILM&nbsp;&nbsp;//</a>
+      <a href="#">&nbsp;&nbsp;VEREIN&nbsp;&nbsp;//</a>
+      <a href="#">&nbsp;&nbsp;KONTAKT&nbsp;&nbsp;//</a>
+      <a href="#">&nbsp;&nbsp;IMPRESSUM </a>
+    </nav>
+  </div> 
 
-      <div id="RandRechts">  
-          <ul class="social">
-          <li id="de_click" class="button_lang current_lang">De</li>
-          <li id="en_click" class="button_lang">En</li>
-         </ul></div>
-    </div>
+  <div id="randRechts">  
+    <button id="de_click" class="button_lang current_lang">DE</button>
+    <button id="en_click" class="button_lang">EN</button>
+  </div>
+</div>
 </header>
