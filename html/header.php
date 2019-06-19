5ffb666dd645103701515345c32a2b171e277299
Nocsis <nicoslentzsch@web.de>
2019-06-19 17:47:16 +0200

    Cleaned up code

--- html/header.php
+++ html/header.php
@@ -2,9 +2,9 @@
 <div class="flex-container">
   <div id="randLinks"></div>
   <div id="ueberleben">Über<strong>leben</strong> </div>
-      
+
   <div id="navigation">
-    <nav>  
+    <nav>
       <a href="#">BELLA&nbsp; //</a>
       <a href="#">&nbsp;&nbsp;JOSEPH &nbsp;//</a>
       <a href="#">&nbsp;&nbsp;NATHAN&nbsp; //</a>
@@ -14,16 +14,16 @@
     </nav>
   </div>
 
-  <div id="navigationRechts"> 
+  <div id="navigationRechts">
     <nav class="sidenav">
       <a href="#"><!--&emsp;&emsp;&emsp;&nbsp;-->FILM&nbsp;&nbsp;//</a>
       <a href="#">&nbsp;&nbsp;VEREIN&nbsp;&nbsp;//</a>
       <a href="#">&nbsp;&nbsp;KONTAKT&nbsp;&nbsp;//</a>
       <a href="#">&nbsp;&nbsp;IMPRESSUM </a>
     </nav>
-  </div> 
+  </div>
 
-  <div id="randRechts">  
+  <div id="randRechts">
     <button id="de_click" class="button_lang current_lang">DE</button>
     <button id="en_click" class="button_lang">EN</button>
   </div>
--- scripts.js
+++ scripts.js
@@ -1,19 +1,19 @@
 "use strict";
 
 var deutsch = document.getElementById('de_click'),
-    english = document.getElementById('en_click'),
-    de_txt = document.querySelectorAll('.de'),
-    en_txt = document.querySelectorAll('.en'),
-    nb_de = de_txt.length,
-    nb_en = en_txt.length;
+  english = document.getElementById('en_click'),
+  de_txt = document.querySelectorAll('.de'),
+  en_txt = document.querySelectorAll('.en'),
+  nb_de = de_txt.length,
+  nb_en = en_txt.length;
 
 deutsch.addEventListener('click', function() {
-    language(deutsch,english);
-    });
+  language(deutsch,english);
+});
 
 english.addEventListener('click', function() {
-    language(english,deutsch);
-    });
+  language(english,deutsch);
+});
 
 init();
 
@@ -23,53 +23,53 @@ window.onscroll = function() {checkScroll()};
 
 
 function checkScroll() {
-    if (document.body.scrollTop > (document.body.scrollHeight / 4) || document.documentElement.scrollTop > (document.documentElement.scrollHeight / 4)) {
-        document.getElementById("upButton").style.display = "block";
-        document.getElementById("downButton").style.display = "none";
-    }
-    else {
-        document.getElementById("upButton").style.display = "none";
-        document.getElementById("downButton").style.display = "block";
-    }
+  if (document.body.scrollTop > (document.body.scrollHeight / 4) || document.documentElement.scrollTop > (document.documentElement.scrollHeight / 4)) {
+    document.getElementById("upButton").style.display = "block";
+    document.getElementById("downButton").style.display = "none";
+  }
+  else {
+    document.getElementById("upButton").style.display = "none";
+    document.getElementById("downButton").style.display = "block";
+  }
 }
 
 function scrollToTop() {
-    window.scroll({top: 0, behavior: 'smooth'});
+  window.scroll({top: 0, behavior: 'smooth'});
 }
 
 function scrollToBottom() {
-    window.scroll({top: document.body.scrollHeight, behavior: 'smooth'});
-    window.scroll({top: document.documentElement.scrollHeight, behavior: 'smooth'});
+  window.scroll({top: document.body.scrollHeight, behavior: 'smooth'});
+  window.scroll({top: document.documentElement.scrollHeight, behavior: 'smooth'});
 }
 
 function init(){
-    language(deutsch,english);
+  language(deutsch,english);
 }
 function language(languageOn,languageOff){
-    if (!languageOn.classList.contains('current_lang')) {
-        languageOn.classList.toggle('current_lang');
-        languageOff.classList.toggle('current_lang');
-        }
+  if (!languageOn.classList.contains('current_lang')) {
+    languageOn.classList.toggle('current_lang');
+    languageOff.classList.toggle('current_lang');
+  }
 
-        if(languageOn.innerHTML == 'DE'){
-        display(de_txt, nb_de);
-        hide(en_txt, nb_en);
-        }
+  if(languageOn.innerHTML == 'DE'){
+    display(de_txt, nb_de);
+    hide(en_txt, nb_en);
+  }
 
-        else if(languageOn.innerHTML == 'EN'){
-        display(en_txt, nb_en);
-        hide(de_txt, nb_de);
-        }   
-    }
+  else if(languageOn.innerHTML == 'EN'){
+    display(en_txt, nb_en);
+    hide(de_txt, nb_de);
+  }   
+}
 
 function display(txt,nb){
-    for(var i=0; i < nb; i++){
-        txt[i].style.display = 'block';
-    }
+  for(var i=0; i < nb; i++){
+    txt[i].style.display = 'block';
+  }
 }
 
 function hide(txt,nb){
-    for(var i=0; i < nb; i++){
-        txt[i].style.display = 'none';
-    }
+  for(var i=0; i < nb; i++){
+    txt[i].style.display = 'none';
+  }
 }       
