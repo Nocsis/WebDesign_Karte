4de6ab7acbef994295d037cec6cddba459d85f9c
Nocsis <nicoslentzsch@web.de>
2019-06-23 22:03:55 +0200

    began editing landing page

---
+++ html/index.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html>
+
+  <head>
+    <meta charset="utf-8" />
+    <title>TITLE HERE</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="stylesheet" href="../stylesheet.css">
+  </head>
+
+  <body>
+    <?php include ("../html/header.php"); ?>
+
+    <div id="backgroundVideoContainer">
+      <video autoplay muted loop id="backgroundVideo">
+        <source src="../material/videos/avishay01_sd.mp4" type="video/mp4">
+      </video>
+    </div>
+    <script src="../scripts.js"></script>
+  </body>
+</html>
