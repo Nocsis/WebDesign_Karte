3a875deeea3b7ae63bec915205d390d2be2e3bc2
Natalie <na0312ta@gmail.com>
2019-06-24 23:21:39 +0200

    added association site for IE

---
+++ html/VereinIE.php
@@ -0,0 +1,65 @@
+<!DOCTYPE html>
+<html>
+
+  <head>
+    <meta charset="utf-8" />
+    <title>Impressum</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="stylesheet" href="../stylesheet.css">
+  </head>
+  <body>
+
+      <?php include ("../html/header.php"); ?>
+
+      <div class="flex-container2">
+          <div class="randlinks"></div>
+          <div class="ie-tnz-logo"> <img src="../Material/Bilder/TnZ-Logo.PNG" height="25%" width="35%"> <br> <br> >> Viele Völker werden hingehen und sagen: Kommt, laßt uns <br> hinaufziehen zum Berg des HERRN...
+            Denn von Zion wird <br> Weisung ausgehen und das Wort des HERRN von <br> Jerusalem.<< Jesaja 2:3 </div>
+          <div class="spalte4"></div>
+          <div class="ie-vereinsbilds"style="1px solid purple"><img src="../Material/Bilder/Verein.jpeg" height="100%" width="100%"></div>   
+          <div class="randrechts"></div>
+        </div>
+        <div></div>
+
+       <div class="flex-container3">
+          <div class="randlinks"></div>
+          <div class="ie-strich"> </div>
+          <div class="spalte2"></div>
+          <div class="ie-strich"> </div>
+          <div class="randrechts"></div>
+        </div>
+
+         
+        <div class="container4">
+            <div class="randlinks"></div>
+            <div class="ie-erklärung"> Unsere Arbeit soll ein Tor sein, durch das Versöhnung und <br> 
+              UNterstützung zwischen Deutschen und Israelis, Christen und Juden <br>
+              fließen kann. Wir wünschen uns, dass durch unsere Arbeit viele <br>
+              Menschen mit dem Gott Israels, dem Schöpfer des Universums und <br>
+              König der Welt, in Berührung kommen.</div>
+            <div class="spalte2"></div>
+            <div class="ie-erklärung"> Tor nach Zion e.V. <br> Scharnhorstraße 21 <br> 04275 Leipzig <br> <br> <strong> wwww.tornachzion.de </strong>  </div>
+            <div class="randrechts "></div>
+        </div>
+
+        <div class="container4">
+            <div class="randlinks"></div>
+            <div class="ie-fhd-logo"> <img src="../Material/Bilder/FHD-Logo.PNG" height= 120px; width="210px" ></div>
+            <div class="spalte2"></div>
+            <div class="ie-studenten"> Entworfen von Studierenden der Fachhochschule Dresden <br> <br>
+              Die Fachhochschule Dresden (FHD) leistet mit ihren Bildungsangeboten <br>
+              einen wichtigen Beitrag zur Bewältigung der stetigen Veränderungen <br>
+              und damit der Entwicklung des regionalen Umfeldes, Sachsens und <br>
+              darüber hinaus. Auf wissenschaftlicher Grundlage werden qualifizierte, <br>
+              praxisorientierte und verantwortungsbewusste Absolventen <br>
+              ausgebildet, die in allen Gesellschaftsbereichen eigenständig <br>
+              Problemlösungen entwickeln und vertreten können.<br> <br>
+              <strong>www.fh-dresden.eu</strong>  </div>
+            <div class="randrechts "></div>
+        </div>
+        
+          <script src="../scripts.js"></script>
+      
+  </body>
+
+  </html>
