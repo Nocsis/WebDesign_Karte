1cac5c7f613cfae2cee9e7327d5d96d92e83ae09
Rio <hoehne.henriette@gmail.com>
2019-07-08 15:28:03 +0200

    asd

--- html/stories/joseph.php
+++ html/stories/joseph.php
@@ -1,80 +1,68 @@
 <!DOCTYPE html>
 <html>
+
   <head>
     <meta charset="utf-8" />
-    <title>Tor nach Zion</title>
+    <title>Avishay Golz</title>
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <link rel="stylesheet" href="../stylesheet.css">
-    <script src="../scripts.js"></script>
+    <link rel="stylesheet" href="/stylesheet.css">
   </head>
 
   <body>
-  <?php include ("../html/header.php"); ?>
+    <?php include ("../header.php"); ?>
 
-<div class="flex-container2">
-  <div class="randlinks"></div>
-    <div class="spalte1 IE-name"> 
-      <span id="name1">&nbsp;Josef</span>
-      <span id="name2">&nbsp;Aaron</span>
-   </div>
+    <div class="grid-container" >
 
-   <div class="spalte2"></div>
-   <div class="spalte3">
-     <img src="../material/bilder/portraits/portraitAvishay.jpg" alt="Portrait Avishay" height="100%" width="90.91%">
-   </div>
-    <div class="spalte4"></div>
-    <div class="IE-text de"> 	„Ich erzähle meine Geschichte. Weiter und weiter und es ist, … es ist schwer. Und es tut weh. Aber ich freue mich heute, dass ich die Geschichte  erzählen und weitergeben kann.“</div>
-    <div class="IE-text en"> „I am going on telling my story. It is hard and it hurts. But I am happy that I can tell my story and share it with others.”</div>
-    <div class="randrechts"></div>
-</div>
+      <div id="name">
+        <span id="name1">
+          &nbsp;Avishay
+        </span>
+        <span id="name2">
+          &nbsp;Golz
+        </span>
+        <span class="de" id="name3">
+          &nbsp;über
+        </span>
+        <span class="en" id="name3">
+          &nbsp;talking about
+        </span>
+        <span class="de" id="name4">
+          &nbsp;seine Mutter
+        </span>
+        <span class="en" id="name4">
+          &nbsp;his mother
+        </span>
+      </div>
 
-  <div class="flex-container3">
-    <div class="leer"></div>
-  </div>
+      <div id="portrait">
+        <img src="/material/bilder/portraits/portraitAvishay.jpg" alt="Portrait Avishay" height="100%" width="90.91%">
+      </div>
 
-  <div class="flex-container4">
-    <div class="randlinks"></div>
-    <div class="strich"></div>
-    <div class="spalte4"></div>
-    <div class="strich2"></div>
-    <div class="randrechts"></div>
-  </div>
+      <div id="text" class="de">
+        >>Wenn du deine Vergangenheit nicht kennst, hast du keine Gegenwart und deine Zukunft ist sehr unklar.<<
+      </div>
 
-  <div class="flex-container5">
-    <div class="randlinks"></div>
-    <div class="IE-erklärung"></div>
-    <div class="spalte4"></div>
-    <div class="IE-jahr de">
-      <strong>//</strong> &nbsp; Geboren am 5.5.1935 in Frankfurt am Main in einer Familie mit 11 Kindern <br>
-      <strong>//</strong> &emsp; 1939 schickt ihn seine Mutter mit zwei Geschwistern nach Frankreich. Dort werden sie an verschiedenen Orten versteckt. <br>
-      <strong>//</strong> &emsp; Nach seiner Befreiung wandert er in Israel ein. Er arbeitet lange Zeit als Kellner. <br></div>
-      <strong>//</strong> &nbsp; Josef Aaron lebt heute in Jerusalem <br>
-    <div class="IE-jahr en">
-      <strong>//</strong> &nbsp; Born on 5.5.1935 in Frankfurt am Main in a family of eleven children <br>
-      <strong>//</strong> &emsp; In 1939 his mother sends him and two other siblings to France. There they are hidden in different places.<br>
-      <strong>//</strong> &emsp; In 1942 the SS finds them and they were brought to Bergen-Belsen.<br>
-      <strong>//</strong> &nbsp; After his liberation and lengthy rehabilitation he immigrates to Israel. He works as a waiter.<br>
-      <strong>//</strong> &nbsp; Josef Aaron lives in Jerusalem today.<br>
+      <div id="text" class="en">
+        >>When you don't know your past, you do not have a present and your future is very unclear.<<
+      </div>
+
+      <div class="s"></div>
+      <div class="s2"></div>
+      <div class="s3"></div>
+      <div class="s4"></div>
+
+      <div id="year" class="de">
+       <strong>// &nbsp; 1930er</strong> &nbsp; Kindheit in Leipzig <br>
+       <strong>// &nbsp; 1939</strong> &emsp; Flucht in die Niederlande bei Kriegsausbruch <br>
+       <strong>// &nbsp; 1945</strong> &emsp; Weg nach Israel <br>
+      </div>
+
+      <div id="year" class="en">
+       <strong>// &nbsp; 1930s</strong> &nbsp; childhood in Leipzig <br>
+       <strong>// &nbsp; 1939</strong> &emsp; escape to the Netherlands at start of war <br>
+       <strong>// &nbsp; 1945</strong> &emsp; path towards Israel <br>
+      </div>
     </div>
-    <div class="randrechts"></div>
-  </div>
-
-
-  <div class="flex-container6">
-      <div class="leer"></div>
-  </div>
-    
-  <div class="flex-container7">
-    <div class="randlinks"></div>
-    <div class="strich3"></div>
-    <div class="spalte4"> </div>
-    <div class="strich4"></div>
-    <div class="randrechts"></div>
-  </div>
-
-  <div class="flex-container8">
-      <div class="leer"></div>
-  </div>
     
   <div id="section2">
     <div class="map-background" id="avishay-map">
