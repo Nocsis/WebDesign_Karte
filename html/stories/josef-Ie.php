5c31919d3cfcd58752c655bcd4c1f478d00bc7eb
Natalie <na0312ta@gmail.com>
2019-07-10 16:35:26 +0200

    finished josef
    button problem again

--- html/stories/josef-Ie.php
+++ html/stories/josef-Ie.php
@@ -16,14 +16,14 @@
       <div class="flex-container2">
         <div class="border-left"></div>
           <div id="ie-name"> 
-            <span id="name1">&nbsp;Joseph</span>
+            <span id="name1">&nbsp;Josef</span>
             <span id="name2">&nbsp;Aaron</span>
          </div> 
 
          <!--Portrait und Text-->
          <div class="column2"></div>
          <div class="column3">
-         <img src="/material/bilder/portraits/portraitAvishay.jpg" alt="Portrait Avishay" height="100%" width="90.91%">
+         <img src="/material/bilder/portraits/portraitJosef.png" alt="Portrait Avishay" height="100%" width="90.91%">
          </div>
           <div class="column4"></div>
           <div class="ie-text de"> „Ich erzähle meine Geschichte. Weiter und weiter und es ist, … es ist schwer. Und es tut weh. Aber ich freue mich heute, dass ich die Geschichte  erzählen und weitergeben kann.“</div>
@@ -94,54 +94,54 @@
         </div>
         <!--Die Reihenfolge der Klassen ist wichtig! Immer "map-button sprache name-stationnr-nr" bzw "map-popup sprache name-stationnr-nr"!-->
         <button class="map-button de josef-station1-1" onmouseover="popupDescription(this)" onmouseout="hideDescription(this)" onclick="popupVideo('/material/videos/josef01_sd.mp4', this)"></button>
-        <div class="map-popup de josef-station1-1"Josef erzählt von seiner Familie in Frankfurt / Main </div>
+        <div class="map-popup de josef-station1-1">Josef erzählt von seiner Familie in Frankfurt / Main </div>
 
-        <button class="map-button de josef-station1-2" onmouseover="popupDescription(this)" onmouseout="hideDescription(this)" onclick="popupVideo('/material/videos/josef02_sd.mp4', this)"></button>
-        <div class="map-popup de josef-station1-2"Josef berichtet von seiner Mutter und der Rettung dreier Kinder.</div>
+        <button class="map-button de josef-station2-1" onmouseover="popupDescription(this)" onmouseout="hideDescription(this)" onclick="popupVideo('/material/videos/josef02_sd.mp4', this)"></button>
+        <div class="map-popup de josef-station2-1">Josef berichtet von seiner Mutter und der Rettung dreier Kinder.</div>
 
-        <button class="map-button de josef-station1-3" onmouseover="popupDescription(this)" onmouseout="hideDescription(this)" onclick="popupVideo('/material/videos/josef03_sd.mp4', this)"></button>
-        <div class="map-popup de josef-station1-3">Josef beschreibt  den Transport ins KZ Bergen-Belsen.</div>
-
-        <button class="map-button de josef-station1-4" onmouseover="popupDescription(this)" onmouseout="hideDescription(this)" onclick="popupVideo('/material/videos/josef04_sd.mp4', this)"></button>
-        <div class="map-popup de josef-station1-4">Josef erzählt von seinen schrecklichen Erlebnissen im Konzentrationslager.</div>
-
-        <button class="map-button de josef-station2-1" onmouseover="popupDescription(this)" onmouseout="hideDescription(this)" onclick="popupVideo('/material/videos/josef04_sd.mp4', this)"></button>
-        <div class="map-popup de bella-station2-1">Josef findet nach seinen traumatischen Erlebnissen langsam zurück ins Leben.</div>
+        <button class="map-button de josef-station2-2" onmouseover="popupDescription(this)" onmouseout="hideDescription(this)" onclick="popupVideo('/material/videos/josef03_sd.mp4', this)"></button>
+        <div class="map-popup de josef-station2-2">Josef beschreibt  den Transport ins KZ Bergen-Belsen.</div>
 
         <button class="map-button de josef-station3-1" onmouseover="popupDescription(this)" onmouseout="hideDescription(this)" onclick="popupVideo('/material/videos/josef04_sd.mp4', this)"></button>
-        <div class="map-popup de bella-station3-1">Josef berichtet von den schweren Anfangsjahren in Israel.</div>
+        <div class="map-popup de josef-station3-1">Josef erzählt von seinen schrecklichen Erlebnissen im Konzentrationslager.</div>
 
         <button class="map-button de josef-station3-2" onmouseover="popupDescription(this)" onmouseout="hideDescription(this)" onclick="popupVideo('/material/videos/josef04_sd.mp4', this)"></button>
-        <div class="map-popup de josef-station3-2">Josef erzählt von seinem Leben in Jerusalem.</div>
+        <div class="map-popup de bella-station3-2">Josef findet nach seinen traumatischen Erlebnissen langsam zurück ins Leben.</div>
 
-        <button class="map-button de josef-station3-3" onmouseover="popupDescription(this)" onmouseout="hideDescription(this)" onclick="popupVideo('/material/videos/josef04_sd.mp4', this)"></button>
-        <div class="map-popup de josef-station3-3"Josef erinnert sich an seine Mutter und ihr Vermächtnis in seinem Leben</div>
+        <button class="map-button de josef-station4-1" onmouseover="popupDescription(this)" onmouseout="hideDescription(this)" onclick="popupVideo('/material/videos/josef04_sd.mp4', this)"></button>
+        <div class="map-popup de bella-station4-1">Josef berichtet von den schweren Anfangsjahren in Israel.</div>
+
+        <button class="map-button de josef-station4-2" onmouseover="popupDescription(this)" onmouseout="hideDescription(this)" onclick="popupVideo('/material/videos/josef04_sd.mp4', this)"></button>
+        <div class="map-popup de josef-station4-2">Josef erzählt von seinem Leben in Jerusalem.</div>
+
+        <button class="map-button de josef-station4-3" onmouseover="popupDescription(this)" onmouseout="hideDescription(this)" onclick="popupVideo('/material/videos/josef04_sd.mp4', this)"></button>
+        <div class="map-popup de josef-station4-3">Josef erinnert sich an seine Mutter und ihr Vermächtnis in seinem Leben</div>
         
 
 
         <button class="map-button en josef-station1-1" onmouseover="popupDescription(this)" onmouseout="hideDescription(this)" onclick="popupVideo('/material/videos/josef01_sd_en.mp4', this)"></button>
         <div class="map-popup en josef-station1-1">Josef talks about his family in Frankfurt/Main. </div>
 
-        <button class="map-button en josef-station1-2" onmouseover="popupDescription(this)" onmouseout="hideDescription(this)" onclick="popupVideo('/material/videos/josef02_sd_en.mp4', this)"></button>
-        <div class="map-popup en josef-station1-2">Josef talks about his mother and the rescue of him and his two siblings.</div>
+        <button class="map-button en josef-station2-1" onmouseover="popupDescription(this)" onmouseout="hideDescription(this)" onclick="popupVideo('/material/videos/josef02_sd_en.mp4', this)"></button>
+        <div class="map-popup en josef-station2-1">Josef talks about his mother and the rescue of him and his two siblings.</div>
 
-        <button class="map-button en josef-station1-3" onmouseover="popupDescription(this)" onmouseout="hideDescription(this)" onclick="popupVideo('/material/videos/josef03_sd_en.mp4', this)"></button>
-        <div class="map-popup en josef-station1-3">Josef describes the transport to the concentration camp Bergen-Belsen.</div>
-
-        <button class="map-button en josef-station1-4" onmouseover="popupDescription(this)" onmouseout="hideDescription(this)" onclick="popupVideo('/material/videos/josef04_sd_en.mp4', this)"></button>
-        <div class="map-popup en josef-station1-4">Josef talks about the horrifying experiences in the concentration camp.</div>
-
-        <button class="map-button en josef-station2-1" onmouseover="popupDescription(this)" onmouseout="hideDescription(this)" onclick="popupVideo('/material/videos/josef04_sd_en.mp4', this)"></button>
-        <div class="map-popup en josef-station2-1">Josef finds his way back to life after his very traumatic experiences. </div>
+        <button class="map-button en josef-station2-2" onmouseover="popupDescription(this)" onmouseout="hideDescription(this)" onclick="popupVideo('/material/videos/josef03_sd_en.mp4', this)"></button>
+        <div class="map-popup en josef-station2-2">Josef describes the transport to the concentration camp Bergen-Belsen.</div>
 
         <button class="map-button en josef-station3-1" onmouseover="popupDescription(this)" onmouseout="hideDescription(this)" onclick="popupVideo('/material/videos/josef04_sd_en.mp4', this)"></button>
-        <div class="map-popup en josef-station3-1">Josef talks about his first  years in Israel and his hardships.</div>
+        <div class="map-popup en josef-station3-1">Josef talks about the horrifying experiences in the concentration camp.</div>
 
         <button class="map-button en josef-station3-2" onmouseover="popupDescription(this)" onmouseout="hideDescription(this)" onclick="popupVideo('/material/videos/josef04_sd_en.mp4', this)"></button>
-        <div class="map-popup en josef-station3-2">Josef talks about his life in in Jerusalem.</div>
+        <div class="map-popup en josef-station3-2">Josef finds his way back to life after his very traumatic experiences. </div>
 
-        <button class="map-button en josef-station3-3" onmouseover="popupDescription(this)" onmouseout="hideDescription(this)" onclick="popupVideo('/material/videos/josef04_sd_en.mp4', this)"></button>
-        <div class="map-popup en josef-station3-3">Josef remembers his mother and her legacy in his life</div>
+        <button class="map-button en josef-station4-1" onmouseover="popupDescription(this)" onmouseout="hideDescription(this)" onclick="popupVideo('/material/videos/josef04_sd_en.mp4', this)"></button>
+        <div class="map-popup en josef-station-4-1">Josef talks about his first  years in Israel and his hardships.</div>
+
+        <button class="map-button en josef-station4-2" onmouseover="popupDescription(this)" onmouseout="hideDescription(this)" onclick="popupVideo('/material/videos/josef04_sd_en.mp4', this)"></button>
+        <div class="map-popup en josef-station4-2">Josef talks about his life in in Jerusalem.</div>
+
+        <button class="map-button en josef-station4-3" onmouseover="popupDescription(this)" onmouseout="hideDescription(this)" onclick="popupVideo('/material/videos/josef04_sd_en.mp4', this)"></button>
+        <div class="map-popup en josef-station4-3">Josef remembers his mother and her legacy in his life</div>
       </div>
     </div>
 
