46930cffd2268f51bdee00bf2472b31509b12552
Nocsis <nicoslentzsch@web.de>
2019-07-11 09:35:54 +0200

    implemented Nata's flexbox layout into contact.php, needs new styling

--- html/contact.php
+++ html/contact.php
@@ -14,29 +14,44 @@
 
     <div class="grid-container">
 
-      <div id="contact-wrapper">
-        <div id="contact-text">
-          <h3>Kontaktaufnahme</h3>
-          <p>Zur Kontaktaufnahme mit dem Verein „Tor nach Zion e.V.“ nutzen Sie bitte dieses Kontaktformular.</p>
+      <div class="contact-wrapper">
+        <div class="contact-text-wrapper">
+          <div class="contact-text-spacer">
+            <div class="contact-text">
+              <h3>Kontaktaufnahme</h3>
+              <p>Zur Kontaktaufnahme mit dem Verein „Tor nach Zion e.V.“ nutzen Sie bitte dieses Kontaktformular.</p>
+            </div>
+          </div>
         </div>
 
-        <form id="contact-form" action="sendmail.php" method='post'>
-          <div id="contact-flex-spacer">
-            <div id="contact-flex-spacer1">
-              <div id="contact-flex-spacer2">
-                <input id="contact-name" name="name" type="text" tabindex="1" placeholder="Ihr Name..." required autofocus>
-                <input id="contact-email" name="email" type="email" tabindex="2" placeholder="Ihre Email-Adresse..." required>
+        <form class="contact-form" action="sendmail.php" method='post'>
+          <div class="contact-flex-wrapper">
+            <div class="contact-confirmation-wrapper">
+              <div class="contact-flex-wrapper2">
+                <div class="contact-name-wrapper">
+                  <div class="contact-name-label contact-label">Name</div>
+                  <input class="contact-name" name="name" type="text" tabindex="1" placeholder="Ihr Name..." required autofocus>
+                </div>
+                <div class="contact-email-wrapper">
+                  <div class="contact-email-label contact-label">E-Mail</div>
+                  <input class="contact-email" name="email" type="email" tabindex="2" placeholder="Ihre Email-Adresse..." required>
+                </div>
               </div>
-
-              <div id="contact-confirmation"><p>Nachricht gesendet!</p></div>
+              <div class="contact-confirmation"><p>Nachricht gesendet!</p></div>
             </div>
 
-            <input id="contact-subject" name="subject" type="text" tabindex="3" placeholder="Betreff der Nachricht..." required>
-            <textarea id="contact-message" name="message" tabindex="4" placeholder="Ihre Nachricht..." required></textarea>
+            <div class="contact-subject-wrapper">
+              <div class="contact-subject-label contact-label">Betreff</div>
+              <input class="contact-subject" name="subject" type="text" tabindex="3" placeholder="Betreff der Nachricht..." required>
+            </div>
+            <div class="contact-message-wrapper">
+              <div class="contact-message-label contact-label"></div>
+              <textarea class="contact-message" name="message" tabindex="4" placeholder="Ihre Nachricht..." required></textarea>
+            </div>
           </div>
-          <div id="contact-flex-spacer3">
-            <label for="contact-submit">// Senden</label>
-            <button name="submit" id="contact-submit" type="submit" tabindex="5"></button>
+          <div class="contact-submit-wrapper">
+            <p class="contact-submit-text">// Senden</p>
+            <button class="contact-submit" name="submit" type="submit" tabindex="5"></button>
           </div>
         </form>
       </div>
